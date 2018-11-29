4b7571b31091d3771e4bcc8487b91b8d489bbcb1
aydın gültepe <aydngltp@gmail.com>
2018-11-30 00:45:22 +0300

    Bağış Controller oluşturuldu.

--- SYP/Controllers/MuhtacController.cs
+++ SYP/Controllers/MuhtacController.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using SYP.Filtreler;
 using SYP.Models;
 
 namespace SYP.Controllers
@@ -20,6 +21,33 @@ namespace SYP.Controllers
             return View(db.Muhtaclar.ToList());
         }
 
+        public ActionResult List(int? id, string q)
+        {
+            var muhtaclar = db.Muhtaclar.Where(i => i.AdminOnay == true).AsQueryable();
+
+            if (id != null)
+            {
+                if (id == 6)
+                {
+                    muhtaclar = db.Muhtaclar.Where(i => i.Aciliyet == 5).Where(i => i.AdminOnay == true);
+                }
+                else
+                {
+                    muhtaclar = db.Muhtaclar.Where(i => i.YardimTuru.Id == id).Where(i => i.AdminOnay == true);
+                }
+            }
+            if (string.IsNullOrEmpty(q) == false)
+            {
+                muhtaclar = muhtaclar.Where(i => i.Baslik.Contains(q) || i.Aciklama.Contains(q));
+            }
+
+            return View(muhtaclar.Include(i => i.Adres).Include(i => i.Il).ToList());
+        }
+
+
+
+        [HttpGet]
+        //[Route("Home/Detay/{id}")]
         // GET: Muhtac/Details/5
         public ActionResult Details(int? id)
         {
@@ -27,17 +55,40 @@ namespace SYP.Controllers
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Muhtac muhtac = db.Muhtaclar.Find(id);
+            //Blog blog = db.Bloglar.Find(id);
+            var muhtac = db.Muhtaclar.Where(i => i.Id == id).FirstOrDefault();
+            //Muhtac muhtac = db.Muhtaclar.Include(b => b.Adres).Include(b=>b.Il).FirstOrDefault(b => b.Id == id);
+            //Find metodu çalışmadı onun yerine firstofdefault kullanıldı.
+
             if (muhtac == null)
             {
                 return HttpNotFound();
             }
+            //ViewBag.KategoriAdi = blog.CategoryId;
+            //ViewBag.KategoriId = new SelectList(db.Kategoriler, "Id", "KategoriAdi",blog.CategoryId);
             return View(muhtac);
+
         }
 
         // GET: Muhtac/Create
+        [GirisKontrolFiltresi]
+        [HttpGet]
         public ActionResult Create()
         {
+            List<SelectListItem> yardimturleri = (from i in db.YardimTurler.ToList()
+                                                  select new SelectListItem
+                                                  {
+                                                      Text = i.YardimTuruAdi,
+                                                      Value = i.Id.ToString()
+                                                  }).ToList();
+            ViewBag.YardimTurleri = yardimturleri;
+
+            List<SelectListItem> iller = db.Iller.Select(p => new SelectListItem
+            {
+                Text = p.IlAdi,
+                Value = p.Id.ToString()
+            }).ToList();
+            ViewBag.Iller = iller;
             return View();
         }
 
@@ -46,15 +97,49 @@ namespace SYP.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "Id,Baslik,MuhtacAdiSoyadi,Aciklama,Lat,Lng,EklenmeTarihi,Aciliyet,YardimYapildimi,AdminOnay")] Muhtac muhtac)
+        [GirisKontrolFiltresi]
+        public ActionResult Create(Muhtac muhtac)
         {
+
             if (ModelState.IsValid)
             {
-                db.Muhtaclar.Add(muhtac);
+                int? kullaniciId = (Int32)Session["uyeid"];
+                var yenimuhtac = new Muhtac()
+                {
+                    Aciklama = muhtac.Aciklama,
+                    Aciliyet = muhtac.Aciliyet,
+                    Adres = muhtac.Adres,
+                    Baslik = muhtac.Baslik,
+                    EklenmeTarihi = DateTime.Today,
+                    Kullanici = db.Kullanicilar.FirstOrDefault(p => p.Id == kullaniciId),
+                    MuhtacAdiSoyadi = muhtac.MuhtacAdiSoyadi,
+                    YardimTuru = db.YardimTurler.FirstOrDefault(p => p.Id == muhtac.YardimTuru.Id),
+                    Il = db.Iller.FirstOrDefault(p => p.Id == muhtac.Il.Id)
+                };
+                //yenimuhtac.AdminOnay = false; gerek var mı?
+                db.Muhtaclar.Add(yenimuhtac);
                 db.SaveChanges();
+
                 return RedirectToAction("Index");
             }
 
+
+            List<SelectListItem> yardimturleri = (from i in db.YardimTurler.ToList()
+                                                  select new SelectListItem
+                                                  {
+                                                      Text = i.YardimTuruAdi,
+                                                      Value = i.Id.ToString()
+                                                  }).ToList();
+            ViewBag.YardimTurleri = yardimturleri;
+
+            List<SelectListItem> iller = db.Iller.Select(p => new SelectListItem
+            {
+                Text = p.IlAdi,
+                Value = p.Id.ToString()
+            }).ToList();
+            ViewBag.Iller = iller;
+
+            TempData["eklendi"] = "İhtiyaç Sahibi Eklendi";
             return View(muhtac);
         }
 
